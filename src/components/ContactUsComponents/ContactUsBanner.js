ee6088785b01e35a1db7f631fdc6c3b7ea339ecc
Christopher Zou <cwzou@berkeley.edu>
2021-01-18 23:51:35 -0800

    Finish contact page kinda

---
+++ src/components/ContactUsComponents/ContactUsBanner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from '../Banner/Banner'
+
+const ContactUsBanner = () => {
+    return (
+        <div>
+            <Banner title="CONTACT US" message="We’re always on the lookout for partners and projects. This page contains work samples and an FAQ. 
+            If you’re interested in getting in touch, please fill out the contact form below." buttonMsg="Contact Form"
+            bg="white">
+            </Banner>
+        </div>
+    )
+}
+
+export default ContactUsBanner;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,2 +1,13 @@
 import React from 'react'
+import ContactUsBanner from '../components/ContactUsComponents/ContactUsBanner'
+import Layout from '../components/Layout/Layout'
 
+const ContactUs = () => {
+    return (
+        <Layout light={false}>
+            <ContactUsBanner/>
+        </Layout>
+    )
+}
+
+export default ContactUs;
