e472f1ff1cc0f85d20e27ae6afd6e75691b506a3
Oliver Ye <oliverye@berkeley.edu>
2022-01-08 13:11:11 -0800

    renamed file, moved to About folder

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ import AboutImgBanner from '../components/AboutComponents/AboutImgBanner'
 import AboutExperience from '../components/AboutComponents/AboutExperience'
 import AboutBeyond from '../components/AboutComponents/AboutBeyond'
 import AboutSponsors from '../components/AboutComponents/AboutSponsors'
-import Members from '../components/Members/Members'
+import AboutMembers from '../components/AboutComponents/AboutMembers'
 
 const About = () => {
     return (
@@ -18,7 +18,7 @@ const About = () => {
             <AboutMission/>
             <AboutExperience/>
             <AboutImgBanner/>
-            <Members/>
+            <AboutMembers/>
             <AboutBeyond/>
             <AboutSponsors/>
         </Layout>
