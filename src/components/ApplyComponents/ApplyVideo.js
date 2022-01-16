0fbbd1a2eacde8a950b9faa91d31860cadec278c
aprilhuang333 <97777029+aprilhuang333@users.noreply.github.com>
2022-01-16 03:20:54 -0800

    Updated Recruitment Video

--- src/components/ApplyComponents/ApplyVideo.js
+++ src/components/ApplyComponents/ApplyVideo.js
@@ -7,7 +7,7 @@ const ApplyVideo = () => {
             <InfoSec subtitle="What is it like to be in Phoenix?" 
             background="gray" alignment="center">
             <div style={{textAlign: "center"}}>
-            <iframe width="854" height="480" src="https://www.youtube.com/embed/JpnflOvUUX8"
+            <iframe width="854" height="480" src="https://youtu.be/eiuG1SOJh7s"
             frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
             allowfullscreen></iframe>
             </div>
