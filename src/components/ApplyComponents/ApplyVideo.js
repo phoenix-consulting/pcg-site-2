8dc57029747733f9f9e640618b8bd86f03e331b6
aprilhuang333 <97777029+aprilhuang333@users.noreply.github.com>
2022-01-18 01:21:20 -0800

    s22 video update try 2

--- src/components/ApplyComponents/ApplyVideo.js
+++ src/components/ApplyComponents/ApplyVideo.js
@@ -7,8 +7,8 @@ const ApplyVideo = () => {
             <InfoSec subtitle="What is it like to be in Phoenix?" 
             background="gray" alignment="center">
             <div style={{textAlign: "center"}}>
-            <iframe width="854" height="480" src="https://youtu.be/eiuG1SOJh7s"
-            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/eiuG1SOJh7s" 
+            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen></iframe>
             </div>
             </InfoSec>
